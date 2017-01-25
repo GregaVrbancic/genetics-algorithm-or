d2b585ccfef0dd3c4f1243b9b078102418ee7f9a
GregaVrbancic <grega.vrbancic@gmail.com>
2017-01-25 16:50:29 +0100

    integrate aurelia client app with bootstrap

--- app/views.py
+++ app/views.py
@@ -1,6 +1,11 @@
 from app import app
+from flask import render_template, send_from_directory
 
 @app.route('/')
 @app.route('/index')
 def index():
-    return "Hello, World!"
+    return render_template('index.html')
+
+@app.route('/scripts/<path:path>')
+def send_js(path):
+    return send_from_directory('../client/scripts', path)
