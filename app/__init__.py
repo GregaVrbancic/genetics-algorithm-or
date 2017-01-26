13e7681e3cf530c78331bf8aca588184f99b6f2b
GregaVrbancic <grega.vrbancic@gmail.com>
2017-01-26 10:49:41 +0100

    add support for websockets

--- app/__init__.py
+++ app/__init__.py
@@ -1,7 +1,10 @@
 from flask import Flask
+import eventlet
 from flask_socketio import SocketIO
 
 app = Flask(__name__)
-socketio = SocketIO(app)
+eventlet.monkey_patch()
+async_mode = 'eventlet'
+socketio = SocketIO(app, async_mode=async_mode)
 
 from app import views
